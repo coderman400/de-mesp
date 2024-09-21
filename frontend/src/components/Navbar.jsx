352886a1b842b5b3ce737c3096c81ddb4f2ac211
arvind <achurox1121@gmail.com>
2024-09-21 23:43:17 +0530

    update form apis

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="bg-white p-4 mx-auto w-4/5 shadow-lg rounded-lg" style={{ position: 'fixed', top: '20px', left: '10%', right: '10%', zIndex: 1000 }}>
             <div className="container flex justify-between items-center">
-                <h1 className="text-black text-2xl font-bold">DeMESP</h1>
+                <h1 className="text-black text-2xl font-bold"><Link to="/">DeMESP</Link></h1>
                 <ul className="flex space-x-4">
                     <li>
                         <Link to="/dashboard" className="text-black hover:bg-blue-700 rounded px-3 py-2">Home</Link>
